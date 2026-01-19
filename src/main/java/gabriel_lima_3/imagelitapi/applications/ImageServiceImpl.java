7ea58c8ec6ead4fdecf2095b202c201c9544ba46
Gabriel <gabriellimma00@gmail.com>
2026-01-19 15:53:20 +0000

    feat: adiciona metodos na interface ImageService e adiciona metodo na claasse ImageServiceImpl

--- src/main/java/gabriel_lima_3/imagelitapi/applications/ImageServiceImpl.java
+++ src/main/java/gabriel_lima_3/imagelitapi/applications/ImageServiceImpl.java
@@ -1,12 +1,14 @@
 package gabriel_lima_3.imagelitapi.applications;
 
 import gabriel_lima_3.imagelitapi.domain.entity.Image;
+import gabriel_lima_3.imagelitapi.domain.enums.ImageExtensions;
 import gabriel_lima_3.imagelitapi.domain.service.ImageService;
 import gabriel_lima_3.imagelitapi.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -25,5 +27,10 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public Optional<Image> getById(UUID id) {
         return repository.findById(id);
+    }
+
+    @Override
+    public List<Image> search(ImageExtensions extensions, String query) {
+        return repository.findByExtensionAndNameOrTagsLike(extensions, query);
     }
 }
--- src/main/java/gabriel_lima_3/imagelitapi/domain/service/ImageService.java
+++ src/main/java/gabriel_lima_3/imagelitapi/domain/service/ImageService.java
@@ -1,6 +1,8 @@
 package gabriel_lima_3.imagelitapi.domain.service;
 import gabriel_lima_3.imagelitapi.domain.entity.Image;
+import gabriel_lima_3.imagelitapi.domain.enums.ImageExtensions;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -9,4 +11,7 @@ public interface ImageService {
     Image save(Image image);
 
     Optional<Image> getById(UUID id);
+
+    List<Image> search (ImageExtensions extensions, String query );
+
 }
